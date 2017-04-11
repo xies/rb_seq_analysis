e5fc84a8addd41268f2fe54b763a08c61bbc6186
xies <xies@mit.edu>
2017-04-10 17:00:48 -0700

    entropies

--- get_alignment_entropy.py
+++ get_alignment_entropy.py
@@ -7,6 +7,8 @@ Created on Tue Apr  4 16:01:53 2017
 """
 
 from Bio import SeqIO, AlignIO, Phylo
+from collections import Counter
+from scipy import stats
 
 fasta_file = '/Users/mimi/Dropbox (Personal)/Mol Bio/DNA sequences/RB family/rb_fastas.fasta'
 
@@ -19,10 +21,38 @@ for f in fasta:
 # Read in the .aln clustal file
 msa_file = '/Users/mimi/Dropbox (Personal)/Mol Bio/DNA sequences/RB family/rb_fastas.aln'
 alignments = AlignIO.read(msa_file, "clustal")
-alignment_array = np.array([list(rec) for rec in alignments], np.character)
+alignments_array = np.array([list(rec) for rec in alignments], np.character)
 
+[num_seq,len_align] = alignments_array.shape
 
 # Read in the .dnd phylogenetic tree file
 dnd_file = '/Users/mimi/Dropbox (Personal)/Mol Bio/DNA sequences/RB family/rb_fastas.dnd'
 tree = Phylo.read(dnd_file,'newick')
+
+# Compute sequence frequencies
+aa_frequencies = [ Counter(alignments_array[:,i]) for i in xrange(len_align) ]
+
+# Throw out '-' and 'X'
+for f in aa_frequencies:
+    try:
+        f.pop('-')
+    except KeyError:
+        pass
+    
+    try:
+        f.pop('X')
+    except KeyError:
+        pass
+                  
+aa_entropies = [ stats.entropy(f.values()) for f in aa_frequencies ]
+
+# RB motif
+motifOI = seqs['HsapRB1'][894:914]
+motif_beg = 1909
+motif_end = 1929
+
+motif_aligned = alignments[:,motif_beg:motif_end]
+AlignIO.write([motif_aligned],
+              '/Users/mimi/Dropbox (Personal)/Mol Bio/DNA sequences/RB family/rb_motif_aligned.aln','clustal')
+
 
